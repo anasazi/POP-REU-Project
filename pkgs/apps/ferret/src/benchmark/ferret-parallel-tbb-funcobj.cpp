5c33a7e5891414a0d2ffcc0e3bb2b7adaed2094a
Eric Reed <neuralnova@gmail.com>
2011-07-21 14:30:34 -0500

    Completed initial translation of ferret to function object TBB version. Not tested yet.

--- pkgs/apps/ferret/src/benchmark/ferret-parallel-tbb-funcobj.cpp
+++ pkgs/apps/ferret/src/benchmark/ferret-parallel-tbb-funcobj.cpp
@@ -1,10 +1,151 @@
 #include "tbb/task_scheduler_init.h"
 #include "tbb/pipeline.h"
 
-#define PIPELINE_WIDTH 1 // TODO pick better value
+#include <stack>
+
+// #define PIPELINE_WIDTH 64
 
 class Read : tbb::filter {
-	void* operator()( void* vitem ) {
+private:
+	//std::stack<DIR> dir_stack;
+	std::stack<stack_data*> stack;
+	const char *starting_dir;
+	char path[ BUFSIZ ];
+	int first_call;
+	int *cnt_enqueue;
+
+	struct stack_data {
+		DIR *dir;
+		char* head;
+	};
+public:
+	Read( const char* starting_dir_, int *cnt_enqueue_ ) :
+		filter( tbb::filter::mode::serial ),
+		starting_dir( starting_dir_ ),
+		first_call(1),
+		cnt_enqueue( cnt_enqueue_ )
+	{};
+
+	virtual ~Read() {
+		// make sure the stack is empty
+		while( ! stack.empty() ) {
+			struct stack_data *x = stack.top();
+			stack.pop();
+			delete x;
+		}
+	};
+
+	void* operator()( void* ) {
+		if( first_call ) {
+			// special handling at the start
+			first_call = 0;
+			path[0] = 0; // empty the path buffer
+			if( ! strcmp( dir, "." ) ) {
+				// if they used the special directory notation,
+				// make sure to enter the current directory before getting an item
+				enter_directory( ".", path );
+				return read_next_item();
+			} else {
+				// otherwise, we can just figure out what to do with the path provided
+				return dispatch( starting_dir, path );
+			}
+		} else {
+			// otherwise, just get the next item
+			return read_next_item();
+		}
+	};
+private:
+	// find the next item
+	load_data* read_next_item() {
+		// if the stack is empty, we are done
+		if( stack.empty() )
+			return NULL;
+		// examine the top of the stack (current directory)
+		struct dirent *ent = NULL;
+		struct stack_data *data = stack.top();
+		// get the next file
+		ent = readdir( data->dir );
+		if( ent == NULL ) {
+			// we've finished the directory! close it and recurse back up the tree
+			data = NULL;
+			leave_directory();
+			return read_next_item();
+		} else {
+			// figure out what to do with this file
+			return dispatch( ent->d_name, data->head );
+		}
+	};
+
+	// decide what to do with a file
+	load_data* dispatch( const char *dir, char *head ) {
+		// if one of the special directories, skip
+		if( dir[0] == '.' && ( dir[1] == 0 || ( dir[1] == '.' && dir[2] == 0 ) ) )
+			return read_next_item(); // i.e. recurse to the next item
+
+		struct stat st;
+		// append the file name to the path
+		strcat( head, dir );
+		assert( stat( path, &st ) != 0 );
+		if( S_ISREG( st.st_mode ) )
+			// just return the loaded file
+			return load_file( path, head );
+		else if( S_ISDIR( st.st_mode ) ) {
+			// append the path separator
+			strcat( head, "/" );
+			// enter the directory
+			enter_directory( path, head + strlen( head ) );
+			// recurse for the next item
+			return read_next_item();
+		}
+	};
+
+	// enter a directory
+	void enter_directory( const char *dir, char *head ) {
+		// open the directory
+		DIR *pd = NULL;
+		assert( pd = opendir( dir ) != NULL );
+		// store the data
+		struct stack_data *data = new stack_data;
+		data->dir = pd;
+		data->head = head;
+		// push onto the stack
+		stack.push( data );
+		return;
+	};
+
+	// leave the directory (now that we've completed it)
+	void leave_directory() {
+		// remove the top of the stack
+		struct stack_data *data = stack.top();
+		stack.pop();
+		// close the directory file descriptor
+		DIR *pd = data->dir;
+		if( pd != NULL )
+			closedir( pd );
+		// remove the appended file name from the path
+		reset_path_head( data->head );
+		delete data;
+		return;
+	};
+
+	// remove the appended part of the path
+	void reset_path_head( char *head ) {
+		head[0] = 0;
+	};
+
+	load_data* load_file( const char *file, char *head ) {
+		// initialize the new token
+		struct load_data *data = new load_data;
+		// copy the file name to the token
+		data->name = strdup( file );
+		// read in the image - assert checks for errors
+		assert( image_read_rgb_hsv( file, &data->width, &data->height, &data->RGB, &data->HSV ) == 0 );
+		// reset the path buffer head
+		reset_path_head( head );
+		// increment enqueued counter
+		(*cnt_enqueue)++;
+		// return a pointer to the data 
+		return data;
 	};
 };
 
@@ -15,6 +156,10 @@ struct load_data {
 };
 
 class SegmentImage : tbb::filter {
+	SegmentImage() :
+		filter( tbb::filter::mode::parallel )
+	{};
+
 	void* operator()( void* vitem ) {
 		struct load_data *load;
 		struct seg_data *seg;
@@ -29,10 +174,10 @@ class SegmentImage : tbb::filter {
 		
 		image_segment( &seg->mask, &seg->nrgn, load->RGB, load->width, load->height );
 
-		//delete[]	load->RGB;
-		free( load->RGB );
-		//delete 		load;
-		free( load );
+		//free( load->RGB );
+		//free( load );
+		delete load->RGB;
+		delete load;
 
 		return (void*) seg;
 	};
@@ -46,6 +191,10 @@ struct seg_data {
 };
 
 class ExtractFeatures : tbb::filter {
+	ExtractFeatures() :
+		filter( tbb::filter::mode::parallel )
+	{};
+
 	void* operator()( void* vitem ) {
 		struct seg_data *seg;
 		struct extract_data *extract;
@@ -56,11 +205,11 @@ class ExtractFeatures : tbb::filter {
 
 		image_extract_helper( seg->HSV, seg->mask, seg->width, seg->height, seg->nrgn, &extract->ds );
 
-		//delete[]	seg->mask;
-		//delete[] 	seg->HSV;
-		free( seg->mask );
-		free( seg->HSV );
-		delete 		seg;
+		//free( seg->mask );
+		//free( seg->HSV );
+		delete seg->mask;
+		delete seg->HSV;
+		delete seg;
 
 		return (void*) extract;
 	};
@@ -72,6 +221,18 @@ struct extract data {
 };
 
 class QueryIndex : tbb::filter {
+private:
+	int *vec_dist_id, *vecset_dist_id, *top_K;
+	cass_table_t *table;
+public:
+	QueryIndex( int *vec_dist_id_, int *vecset_dist_id_, int *top_K_, cass_table_t *table_ ) :
+		filter( tbb::filter::mode::parallel ),
+		vec_dist_id( vec_dist_id_ ),
+		vecset_dist_id( vecset_dist_id_ ),
+		top_K( top_K_ ),
+		table( table_ )
+	{};
+
 	void* operator()( void* vitem ) {
 		struct extract_data *extract;
 		struct vec_query_data *vec;
@@ -86,16 +247,16 @@ class QueryIndex : tbb::filter {
 		query.flags 		= CASS_RESULT_LISTS | CASS_RESULT_USERMEM;
 		vec->ds = query.dataset = &extract->ds;
 		query.vecset_id 	= 0;
-		query.vec_dist_id 	= vec_dist_id; // TODO pass in
-		query.vecset_dist_id	= vecset_dist_id; // TODO pass in
-		query.topk 		= 2 * top_K; // TODO pass in
-		query.extra_params 	= extra_params; // TODO pass in
+		query.vec_dist_id 	= *vec_dist_id; // pass in
+		query.vecset_dist_id	= *vecset_dist_id; // pass in
+		query.topk 		= 2 * *top_K; // pass in
+		query.extra_params 	= extra_params; // pass in
 
 		cass_result_alloc_list( &vec->result, vec->ds->vecset[0].num_regions, query.topk );
 
-		cass_table_query( table, &query, &vec->result ); // TODO pass in
+		cass_table_query( *table, &query, &vec->result ); // pass in
 
-		delete extract; // TODO is this ok?
+		delete extract; 
 
 		return (void*) vec;
 	};
@@ -108,6 +269,18 @@ struct vec_query_data {
 };
 
 class RankCandidates : tbb::filter {
+private:
+	int *vec_dist_id, *vecset_dist_id, *top_K;
+	cass_dataset_t* query_table;
+public:
+	RankCandidates( int *vec_dist_id_, int *vecset_dist_id_, int *top_K_, cass_dataset_t *query_table_ ) :
+		filter( tbb::filter::mode::parallel ),
+		vec_dist_id( vec_dist_id_ ),
+		vecset_dist_id( vecset_dist_id_),
+		top_K( top_K_ ),
+		query_table( query_table_ )
+	{};
+
 	void* operator()( void* vitem ) {
 		struct vec_query_data *vec;
 		struct rank_data *rank;
@@ -122,15 +295,15 @@ class RankCandidates : tbb::filter {
 		query.flags 		= CASS_RESULT_LISTS | CASS_RESULT_USERMEM | CASS_RESULT_SORT;
 		query.dataset 		= vec->ds;
 		query.vecset_id 	= 0;
-		query.vec_dist_id 	= vec_dist_id; // TODO pass in
-		query.vecset_dist_id 	= vecset_dist_id; // TODO pass in
-		query.topk 		= top_K; // TODO pass in
+		query.vec_dist_id 	= *vec_dist_id; // pass in
+		query.vecset_dist_id 	= *vecset_dist_id; // pass in
+		query.topk 		= *top_K; // pass in
 		query.extra_params 	= NULL;
 
-		candidate = cass_result_merge_lists( &vec->result, (cass_dataset_t*) query_table->__private, 0 ); // TODO pass in
+		candidate = cass_result_merge_lists( &vec->result, (cass_dataset_t*) query_table->__private, 0 ); // pass in
 		query.candidate = candidate;
 
-		cass_result_alloc_list( &rank->result, 0, top_K ); // TODO pass in
+		cass_result_alloc_list( &rank->result, 0, *top_K ); // pass in
 		cass_table_query( query_table, &query, &rank->result );
 
 		cass_result_free( &vec->result );
@@ -151,19 +324,32 @@ struct rank_data {
 };
 
 class Write : tbb::filter {
+private:
+	FILE *fout;
+	cass_table_t *query_table;
+	int *cnt_enqueue, *cnt_dequeue;
+public:
+	Write( FILE *fout_, cass_table_t *query_table_, int *cnt_enqueue_, int *cnt_dequeue_ ) :
+		filter( tbb::filter::mode::serial ),
+		fout( fout_ ),
+		query_table( query_table_ ),
+		cnt_enqueue( cnt_enqueue_ ),
+		cnt_dequeue( cnt_dequeue_ )
+	{};
+
 	void* operator()( void* vitem ) {
 		struct rank_data *rank;
 
 		rank = (rank_data*) vitem;
 		assert( rank != NULL );
 
-		fprintf( fout, "%s", rank->name ); // TODO pass in
+		fprintf( fout, "%s", rank->name ); // pass in
 
 		ARRAY_BEGIN_FOREACH(rank->result.u.list, cass_list_entry_t p)
 		{
 			char *obj = NULL;
 			if (p.dist == HUGE) continue;
-			cass_map_id_to_dataobj(query_table->map, p.id, &obj); // TODO pass in
+			cass_map_id_to_dataobj(query_table->map, p.id, &obj); // pass in
 			assert(obj != NULL);
 			fprintf(fout, "\t%s:%g", obj, p.dist);
 		} ARRAY_END_FOREACH;
@@ -171,26 +357,98 @@ class Write : tbb::filter {
 		fprintf( fout, "\n" );
 
 		cass_result_free( &rank->result );
-		delete[] rank->name;
+		delete rank->name;
 		delete rank;
 
-		// TODO add the count printer
+		// add the count printer
+		(*cnt_dequeue)++;
+		fprintf( stderr, "(%d,%d)\n", *cnt_enqueue, *cnt_dequeue );
 
 		return NULL;
 	};
 };
 
 int main( int argc, char *argv[] ) {
-// Setup TODO 
+// Setup 
+	stimer_t tmr;
+
+	int ret, i;
+
+	int cnt_enqueue = 0;
+	int cnt_dequeue = 0;
+
+#ifdef PARSEC_VERSION
+#define __PARSEC_STRING(x) #x
+#define __PARSEC_XSTRING(x) __PARSEC_STRING(x)
+        printf("PARSEC Benchmark Suite Version "__PARSEC_XSTRING(PARSEC_VERSION)"\n");
+        fflush(NULL);
+#else
+        printf("PARSEC Benchmark Suite\n");
+        fflush(NULL);
+#endif //PARSEC_VERSION
+
+	if (argc < 8)
+	{
+		printf("%s <database> <table> <query dir> <top K> <depth> <n> <out>\n", argv[0]); 
+		return 0;
+	}
+
+	char *extra_params = "-L 8 - T 20";
+
+	cass_env_t *env;
+	cass_table_t *table;
+	cass_table_t *query_table;
+
+	int vec_dist_id = 0;
+	int vecset_dist_id = 0;
+
+	const char *db_dir 	= argv[1];
+	const char *table_name	= argv[2];
+	const char *query_dir 	= argv[3];
+
+	int top_K 	= atoi( argv[4] );
+	int num_tokens 	= atoi( argv[5] );
+	int nthreads 	= atoi( argv[6] );
+
+	const char *output_path = argv[7];
+
+	FILE *fout = fopen( output_path, "w" );
+	assert( fout != NULL );
+
+	cass_init();
+
+	if( ( ret = cass_env_open( &env, db_dir, 0 ) ) != 0 ) {
+		printf( "ERROR: %s\n", cass_strerror( ret ) ); 
+		return 0;
+	}
+
+	assert( vec_dist_id = cass_reg_lookup( &env->vec_dist, "L2_float" ) >= 0 );
+
+	assert( vecset_dist_id = cass_reg_lookup( &env->vecset_dist, "emd" ) >= 0 );
+
+	i = cass_reg_lookup( &env->table, table_name );
+	table = query_table = cass_reg_get( &env->table, i );
+
+	if( ( i = table->parent_id ) >= 0 )
+		query_table = cass_reg_get( &env->table, i );
+	
+	if( query_table != table )
+		cass_table_load( query_table );
+
+	cass_map_load( query_table->map );
+	cass_table_load( table );
+
+	image_init( argv[0] );
+
+	stimer_tick( &tmr );
 
 // Create pipeline stages
-// TODO fill in constructors
-	Read 		reader;
-	SegmentImage 	seg;
-	ExtractFeatures	ext;
-	QueryIndex	query;
-	RankCandidates	rank;
-	Write		write;
+	Read 		reader( query_dir, &cnt_enqueue );
+	SegmentImage 	seg();
+	ExtractFeatures	ext();
+	QueryIndex	query( &vec_dist_id, &vecset_dist_id, &top_K, table );
+	RankCandidates	rank( &vec_dist_id, &vecset_dist_id, &top_K, query_table );
+	Write		write( fout, query_table, &cnt_enqueue, &cnt_dequeue );
 	tbb::pipeline 	pipe;
 
 	pipe.add_filter( reader );
@@ -201,7 +459,25 @@ int main( int argc, char *argv[] ) {
 	pipe.add_filter( write );
 
 // Run pipeline
-	pipe.run( PIPELINE_WIDTH );
+	//pipe.run( PIPELINE_WIDTH );
+	pipe.run( num_tokens );
 
-// Clean up TODO
+// Clean up 
+	assert( cnt_enqueue == cnt_dequeue );
+	pipe.clear();
+
+	stimer_tuck( &tmr, "QUERY TIME" );
+
+	if( ( ret = cass_env_close( env, 0 ) ) != 0 ) {
+		printf( "ERROR: %s\n", cass_strerror( ret ) );
+		return 0;
+	}
+
+	cass_cleanup();
+
+	image_cleanup();
+
+	fclose( fout );
+
+	return 0;
 }
